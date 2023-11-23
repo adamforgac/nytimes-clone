d16af46abedc75e1406a541320bbd2907387380c
Adam Forgac <aforg@email.cz>
2023-11-23 15:11:22 +0100

    Create the first section articles
    The page is now done from one ad banner on top, to another ad banner
    somewhere in the middle of the page

--- main.js
+++ main.js
@@ -3,9 +3,6 @@ var __webpack_exports__ = {};
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-console.log('hello');
-alert('hello');
-console.log("Hello for second time");
+
 /******/ })()
 ;
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBQSxPQUFPLENBQUNDLEdBQUcsQ0FBQyxPQUFPLENBQUM7QUFDcEJDLEtBQUssQ0FBQyxPQUFPLENBQUM7QUFDZEYsT0FBTyxDQUFDQyxHQUFHLENBQUMsdUJBQXVCLENBQUMsQyIsInNvdXJjZXMiOlsid2VicGFjazovL293bi1wcm9qZWN0LXRlbXBsYXRlLy4vc3JjL2luZGV4LmpzIl0sInNvdXJjZXNDb250ZW50IjpbImNvbnNvbGUubG9nKCdoZWxsbycpO1xuYWxlcnQoJ2hlbGxvJyk7XG5jb25zb2xlLmxvZyhcIkhlbGxvIGZvciBzZWNvbmQgdGltZVwiKTtcbiJdLCJuYW1lcyI6WyJjb25zb2xlIiwibG9nIiwiYWxlcnQiXSwic291cmNlUm9vdCI6IiJ9
